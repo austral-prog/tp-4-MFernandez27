32f5f77928094ab0eb8244360c6a2a5cd47e53f1
MFernandez27 <mfernandez27@mail.austral.edu.ar>
2025-04-14 18:51:33 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,8 +1,8 @@
 def leap_year():
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
-		print("True")
-	else:
-		print("False")
+	    print("True")
+    else:
+	    print("False")
 
 ano = int(input("Ingrese un año: "))
 
